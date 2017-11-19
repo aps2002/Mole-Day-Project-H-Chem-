3ad5131c281bf70211684ab207634117796b34d2
Apaar Bhatnagar <apaarbh@gmail.com>
2017-11-19 17:50:18 -0500

    Mole Day PacMan Game
    A game created for my Honors Chemistry Class,received a 100:)!

---
+++ Pac-Man-master/src/pacman/Main.java
@@ -0,0 +1,39 @@
+package pacman;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Main.fx created on 2008-12-20, 12:02:26 <br>
+ * Main.java created October 2017
+ * @author Apaar Bhatnagar
+ */
+public class Main extends Application {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    Application.launch(Main.class, args);
+  }
+
+  @Override
+  public void start(Stage primaryStage) {
+    primaryStage.setTitle("Pac-Mole Coded By APAAR BHATNAGAR");
+    primaryStage.setWidth(MazeData.calcGridX(MazeData.GRID_SIZE_X + 2));
+    primaryStage.setHeight(MazeData.calcGridY(MazeData.GRID_SIZE_Y + 4));
+//    scene: Scene{
+//            content: [ Maze {}
+//            ]
+//           }
+
+    final Group root = new Group();
+    final Scene scene = new Scene(root);
+    root.getChildren().add(new Maze());
+    primaryStage.setScene(scene);
+    primaryStage.show();
+  }
+
+}
